8860cb070130e9587d7e75d444a1d3777cbf6659
mvraa <m.vraa@live.dk>
2021-03-06 22:37:22 +0100

    new page

--- index.js
+++ index.js
@@ -3,8 +3,16 @@ const app = express();
 const importData = require("./data.json");
 let port = process.env.PORT || 3000;
 
+// router:
+var mypageRouter = require('./routes/mypage');
+app.use('/mypage', mypageRouter);
+
 app.get("/", (req, res) => {
     res.send("Mathias Vraa");
+
+    res.render('mypage', {
+        title: 'My List Page'
+    });
 });
 
 app.get("/players", (req, res) => {
@@ -12,5 +20,5 @@ app.get("/players", (req, res) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app is listening on localhost:${port}`);
+    console.log(`Example app is listening on localhost: ${port}`);
 });
