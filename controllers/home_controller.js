f4e585dd7e7b8ab50203c2e73072202ea4ac0241
mvraa <m.vraa@live.dk>
2021-03-06 22:17:14 +0100

    Revert "test"
    This reverts commit d8cff44605fedc055f9a46f6c838256a553a865d.

--- controllers/home_controller.js
+++
@@ -1,6 +0,0 @@
-/* GET home page */
-module.exports.homePage = function (req, res) {
-    res.render('index', {
-        title: 'Lab 2a - ejs'
-    });
-}
--- routes/index.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const homeController = require('../controllers/home_controller')
-
-/* GET home page. */
-router.get('/', homeController.homePage);
-
-module.exports = router;
