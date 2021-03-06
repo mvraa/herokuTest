6c1f3b39a120ef3351ab0b8fd11284b15f33dac1
mvraa <m.vraa@live.dk>
2021-03-06 22:47:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 const express = require("express");
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
 const importData = require("./data.json");
 let port = process.env.PORT || 3000;
 
-// router:
-const layouts = require("express-ejs-layouts");
-app.use(layouts);
-var mypageRouter = require('./routes/mypage');
-app.use('/mypage', mypageRouter);
-
 app.get("/", (req, res) => {
     res.send("Mathias Vraa");
-
-
 });
 
 app.get("/players", (req, res) => {
